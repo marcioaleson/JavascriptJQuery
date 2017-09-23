063ca8affd455f79b63ea4bb21983943f2613a1d
marcioaleson <marcioaleson@gmail.com>
2017-09-22 21:49:49 -0300

    JavaScript Validation Form Example.

---
+++ scriptformvalidation/action_page_post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome.</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen,projection">
+  </head>
+  <body>
+    <h2> Welcome <?php echo $_POST["fname"]; ?></h2>
+  <!-- JQuery -->
+  <script src="js/jquery-3.1.0.min.js"></script>
+  </body>
+</html>
